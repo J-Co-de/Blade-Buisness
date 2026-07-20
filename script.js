bf0d51c4885b84b67be7d304682736da8106f21f
J-Co-de <jclark3987@gmail.com>
2026-07-20 13:21:35 -0500

    Enhance navigation styles; implement responsive typography; adjust header structure and functionality

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 // ===============================
 const nav = document.querySelector(".navbar");
 const header = document.querySelector("header");
-
-const getHeroHeight= () => header.style.height = `${window.innerHeight - nav.getBoundingClientRect().height}px`;
+const arrowObject = document.querySelector(".arrow-object");
+const getHeroHeight = () =>
+  header.style.height = `${window.innerHeight - nav.getBoundingClientRect().height}px`;
 getHeroHeight();
+const getArrowHeight = () => {
+    arrowObject.style.height = `${nav.getBoundingClientRect().height}px`;
+}
+getArrowHeight();
