dfcc8207728d23de34536a2a9e25be84df5a5e72
J-Co-de <jclark3987@gmail.com>
2026-07-21 14:40:37 -0500

    Refactor hero section styles; add buttons and stats display; improve responsive design

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const header = document.querySelector("header");
 const getHeroHeight = () => {
   const navHeight = nav.getBoundingClientRect().height;
   header.style.height = `${window.innerHeight - navHeight}px`;
-  header.style.marginTop = `${navHeight}px`
+  header.style.marginTop = `${navHeight}px`;
 };
 getHeroHeight();
+
+window.visualViewport.addEventListener("resize", () => {
+  getHeroHeight;
+});
