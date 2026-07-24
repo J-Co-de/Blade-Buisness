fd9486988a6dd968f037a7c1be907c7a5297ba19
J-Co-de <jclark3987@gmail.com>
2026-07-24 11:30:06 -0500

    Update hero section styles for improved responsiveness; replace vh with lvh units in CSS and fix function call in script.js

--- script.js
+++ script.js
@@ -3,13 +3,21 @@
 // ===============================
 const nav = document.querySelector("nav");
 const header = document.querySelector("header");
+
 const getHeroHeight = () => {
   const navHeight = nav.getBoundingClientRect().height;
   header.style.height = `${window.innerHeight - navHeight}px`;
   header.style.marginTop = `${navHeight}px`;
 };
-getHeroHeight();
 
+function setRealVH() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+getHeroHeight();
+setRealVH();
 window.visualViewport.addEventListener("resize", () => {
-  getHeroHeight;
+  getHeroHeight();
+  setRealVH();
 });
