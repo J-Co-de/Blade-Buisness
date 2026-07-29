22996751e840dd45e3e08aadf698f9cbc3030b8a
J-Co-de <jclark3987@gmail.com>
2026-07-29 17:37:34 -0500

    I added a drop down menu for mobile and fixed other minor issues.

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // ===============================
 // Element References
 // ===============================
+const hamburger = document.querySelector(".hamburger");
+const hamburgerOpen = document.querySelector(".hamburger-open");
+const hamburgerExit = document.querySelector(".hamburger-exit");
+const links = document.querySelector(".links");
+const hr = document.querySelector("nav hr");
 const nav = document.querySelector("nav");
-const main = document.querySelector("main");
-
-function setNavOffset() {
-  const navHeight = nav.offsetHeight;
-  document.documentElement.style.setProperty("--nav-height", `${navHeight}px`);
-  console.log(navHeight);
-}
-setNavOffset();
-
-const viewport = window.visualViewport ?? window;
-viewport.addEventListener("resize", () => {
-  setNavOffset();
+const navContainer = document.querySelector(".nav-content");
+hamburger.addEventListener("click", () => {
+  links.classList.toggle("reveal");
+  hr.classList.toggle("reveal");
+  navContainer.classList.toggle("nav-col");
+  hamburgerOpen.classList.toggle("reveal");
+  hamburgerExit.classList.toggle("reveal");
 });
+hamburgerOpen.classList.add("reveal");
+console.log(hamburgerOpen);
+
+function getNavHeight() {
+  document.documentElement.style.setProperty(
+    "--nav-height",
+    nav.offsetHeight + "px",
+  );
+}
+getNavHeight();
