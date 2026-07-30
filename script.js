24f792e6d47e47c2038c0d0d685ac1d1239709d8
J-Co-de <jclark3987@gmail.com>
2026-07-30 11:18:27 -0500

    Accessibility Session: I improved accessibility throughout the HTML file with things like for="" on labels and aria-hidden for decorative svgs

--- script.js
+++ script.js
@@ -4,19 +4,21 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerOpen = document.querySelector(".hamburger-open");
 const hamburgerExit = document.querySelector(".hamburger-exit");
-const links = document.querySelector(".links");
+const navLinks = document.querySelector(".links");
 const hr = document.querySelector("nav hr");
 const nav = document.querySelector("nav");
 const navContainer = document.querySelector(".nav-content");
+let droppedDown = navLinks.classList.contains("reveal");
+
 hamburger.addEventListener("click", () => {
-  links.classList.toggle("reveal");
-  hr.classList.toggle("reveal");
-  navContainer.classList.toggle("nav-col");
-  hamburgerOpen.classList.toggle("reveal");
-  hamburgerExit.classList.toggle("reveal");
+  dropDownToggle();
+  droppedDown = navLinks.classList.contains("reveal");
+  if (droppedDown) {
+    navLinks.addEventListener("click", dropDownToggle);
+  } else {
+    navLinks.removeEventListener("click", dropDownToggle);
+  }
 });
-hamburgerOpen.classList.add("reveal");
-console.log(hamburgerOpen);
 
 function getNavHeight() {
   document.documentElement.style.setProperty(
@@ -24,4 +26,18 @@ function getNavHeight() {
     nav.offsetHeight + "px",
   );
 }
+function setHamburgerAriaExpanded() {
+  const isExpanded = hamburger.getAttribute("aria-expanded") === "true";
+  hamburger.setAttribute("aria-expanded", isExpanded ? "false" : "true");
+}
+function dropDownToggle() {
+  navLinks.classList.toggle("reveal");
+  hr.classList.toggle("reveal");
+  navContainer.classList.toggle("nav-col");
+  hamburgerOpen.classList.toggle("reveal");
+  hamburgerExit.classList.toggle("reveal");
+  getNavHeight();
+  setHamburgerAriaExpanded();
+}
 getNavHeight();
+hamburgerOpen.classList.add("reveal");
